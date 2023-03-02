7edc8d10df7a166f101c8d2f34ee5b91cad6e256
stringthread <string.thread@gmail.com>
2023-03-02 17:44:29 +0900

    実績部分のスクロール同期

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,53 @@
 import "../sass/index.scss";
+import type {} from "typed-query-selector";
 import Swiper, { Navigation, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+
+const setScrollSync = () => {
+  const sectionLeft = document.getElementById("section-works");
+  const sectionRight = document.getElementById("section-works-right");
+  if (!sectionLeft || !sectionRight) {
+    console.error("スクロール同期対象の要素がありません");
+    return;
+  }
+  const sectionLeftItemsMap = new Map<string, HTMLElement>(
+    Array.from(
+      sectionLeft.querySelectorAll(".wrap-card"),
+      (e) =>
+        [e.getAttribute("data-id"), e] as const satisfies readonly [
+          string | null,
+          HTMLElement
+        ]
+    ).filter((v): v is readonly [string, HTMLElement] => !!v[0])
+  );
+  const sectionRightItemsMap = new Map<string, HTMLElement>(
+    Array.from(
+      sectionRight.querySelectorAll(".images-card"),
+      (e) =>
+        [e.getAttribute("data-id"), e] as const satisfies readonly [
+          string | null,
+          HTMLElement
+        ]
+    ).filter((v): v is readonly [string, HTMLElement] => !!v[0])
+  );
+  sectionLeft.addEventListener(
+    "scroll",
+    () => {
+      for (const k of sectionLeftItemsMap.keys()) {
+        const element = sectionLeftItemsMap.get(k);
+        if (element === undefined) continue;
+        const clientRect = element.getBoundingClientRect();
+        if (clientRect.top > 0 && clientRect.top < sectionLeft.clientHeight) {
+          const syncElement = sectionRightItemsMap.get(k);
+          syncElement?.scrollIntoView();
+        }
+      }
+    },
+    false
+  );
+};
 
 window.addEventListener("DOMContentLoaded", () => {
   const swiper = new Swiper(".swiper", {
@@ -19,4 +64,5 @@ window.addEventListener("DOMContentLoaded", () => {
       el: ".swiper-scrollbar",
     },
   });
+  setScrollSync();
 });
