a9c409b10b43c472570b38e53e5bb003a8173661
stringthread <string.thread@gmail.com>
2023-03-02 14:52:57 +0900

    Swiperによるカルーセルの導入

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,7 +1,22 @@
 import "../sass/index.scss";
+import Swiper, { Navigation, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 window.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    const titleDOM = document.getElementById("title");
-    if (titleDOM) titleDOM.innerText = "Modified title";
+  const swiper = new Swiper(".swiper", {
+    modules: [Navigation, Pagination],
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
   });
 });
