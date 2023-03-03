3b952583e1a0983d9abaf88a63c4695fd244cde2
stringthread <string.thread@gmail.com>
2023-03-03 23:23:50 +0900

    実績画像のモーダル化

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -70,6 +70,34 @@ const setScrollSync = () => {
   );
 };
 
+const setOpenWorkImage = () => {
+  if (document.body.clientWidth > 960) return;
+  const right = document.getElementById("section-works-right");
+  const tab = right?.querySelector("h2");
+  const overlay = document.getElementById("modal-overlay");
+  if (!right || !tab || !overlay) {
+    console.error("要素が見つかりませんでした : setOpenWorkImage");
+    return;
+  }
+  const close = () => {
+    right.classList.remove("open");
+    overlay.classList.remove("open");
+  };
+  const open = (e: MouseEvent) => {
+    if (
+      (e.target instanceof Node && tab.contains(e.target)) || // tab子孫クリック
+      right.classList.contains("open") // すでにopen
+    ) {
+      return;
+    }
+    right.classList.add("open");
+    overlay.classList.add("open");
+  };
+  right.addEventListener("click", open, false);
+  tab.addEventListener("click", close, false);
+  overlay.addEventListener("click", close, false);
+};
+
 window.addEventListener("DOMContentLoaded", () => {
   const swiper = new Swiper(".swiper", {
     modules: [Navigation, Pagination],
@@ -86,6 +114,7 @@ window.addEventListener("DOMContentLoaded", () => {
     },
   });
   setScrollSync();
+  setOpenWorkImage();
   const explorer = document.getElementById("explorer");
   const toggleExplorer = document.getElementById("toggle-explorer");
   if (explorer && toggleExplorer) {
